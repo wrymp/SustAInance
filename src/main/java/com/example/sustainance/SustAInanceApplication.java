53cea8f0b9e845ff29041f2706597337e13446b9
admin <vdavi21@freeuni.edu.ge>
2025-03-26 16:50:15 +0400

    initial commit

---
+++ src/main/java/com/example/sustainance/SustAInanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sustainance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SustAInanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SustAInanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sustainance/SustAInanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sustainance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustAInanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
