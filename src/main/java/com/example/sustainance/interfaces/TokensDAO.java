1f96eb1318fb4ea484bfbba5be08175caf0d5bba
vard0han <vdavi21@freeuni.edu.ge>
2025-06-12 18:23:44 +0400

    deleted unnecessary classes

--- src/main/java/com/example/sustainance/interfaces/TokensDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.sustainance.interfaces;
-
-import com.example.sustainance.models.userAuth.*;
-
-public interface TokensDAO {
-    void addNewToken(createTokenRequest request);
-    getTokenResponse getToken(getTokenRequest request);
-    attemptTokenAuthResponse attemptTokenAuth(attemptTokenAuthRequest request);
-}
